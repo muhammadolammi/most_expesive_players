efc8e3433077f6fea21d214788727729bb7c7665
muhammad_olammi <muhammadolammi@gmail.com>
2023-11-26 11:31:36 +0100

    most ex transfers function

---
+++ most_ex_transfers.js
@@ -0,0 +1,43 @@
+const {JSDOM} = require("jsdom")
+
+async function mostExTransfers(){
+    try{
+        let res = await fetch('https://www.goal.com/en/news/100-most-expensive-football-transfers-all-time/ikr3oojohla51fh9adq3qkwpu')
+        let htmlBody =await res.text()
+        let tableClass = 'tableizer-table'
+        const dom = new JSDOM(htmlBody)
+        let playersDetails = []
+        let table = dom.window.document.querySelector(`table.${tableClass}`)
+        table.querySelectorAll('tr').forEach((tr, trIndex)=>{
+            let trRow ={}
+            tr.querySelectorAll('td').forEach((td, tdIndex)=>{
+                if(tdIndex==0){
+                    trRow['Position'] = td.textContent
+                }
+                if(tdIndex==1){
+                    trRow['Player-Name'] = td.textContent
+                }
+                if(tdIndex==2){
+                    trRow['Clubs'] = td.textContent
+                }
+                if(tdIndex==3){
+                    trRow['Year'] = td.textContent
+                }
+                if(tdIndex==4){
+                    trRow['Price'] = td.textContent
+                }
+
+            });
+        playersDetails.push(trRow)
+        });
+
+        console.log(playersDetails)
+    }catch(err){
+        console.log(`try/catch err ${err.message}`)
+    }
+
+    
+}
+
+mostExTransfers()
+
